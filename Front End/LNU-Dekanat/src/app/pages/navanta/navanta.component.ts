0a172d02cf1773a7c58c5cdf9ed78f0be5b8b508
Mykhailo Kurytsia <m.kurychja@gmail.com>
2025-04-01 21:24:40 +0300

    changes added

---
+++ Front End/LNU-Dekanat/src/app/pages/navanta/navanta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navanta',
+  standalone: true,
+  imports: [],
+  templateUrl: './navanta.component.html',
+  styleUrl: './navanta.component.css'
+})
+export class NavantaComponent {
+
+}
